719172e0c188ea372ee52eda65e0b98546cb3206
Ahmed Ibrahim Ahmed <ahmed7cf@gmail.com>
2023-10-21 11:53:01 +0300

    done

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(function (courseTitle) {
+    let wordsInTheTitle = courseTitle.split(' ')
+    return wordsInTheTitle.map(function (word) {
+      return word.charAt(0).toUpperCase() + word.slice(1)
+    }).join(" ")
+  }
+  )
 }
